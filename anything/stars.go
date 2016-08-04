1b771251a9851085a897c9be5461f60c680470ba
mochi8k <y.hkh.185@gmail.com>
2016-08-04 21:41:56 +0900

    add stars

---
+++ anything/stars.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func from(n int) []int {
+	slice := make([]int, 0)
+	for i := range make([]int, 5) {
+		slice = append(slice, i+1)
+	}
+	return slice
+}
+
+func main() {
+	for _, v := range from(5) {
+		fmt.Println(strings.Repeat("*", v))
+	}
+}
