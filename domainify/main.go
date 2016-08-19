c9029bca72f118838038ad45709876870783bdab
mochi8k <y.hkh.185@gmail.com>
2016-08-19 21:28:38 +0900

    refactoring main.go

--- domainify/main.go
+++ domainify/main.go
@@ -14,6 +14,13 @@ var tlds = []string{"com", "net"}
 
 const allowedChars = "abcdefghijklmnopqrstuvwxyz0123456789_-"
 
+func spaceReplacer(r rune) rune {
+	if unicode.IsSpace(r) {
+		return '-'
+	}
+	return r
+}
+
 func main() {
 
 	if len(os.Args) > 1 {
@@ -23,17 +30,17 @@ func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
+
 		text := strings.ToLower(s.Text())
 		var newText []rune
-		for _, r := range text {
-			if unicode.IsSpace(r) {
-				r = '-'
-			}
 
-			if strings.ContainsRune(allowedChars, r) {
-				newText = append(newText, r)
+		for _, r := range text {
+			v := spaceReplacer(r)
+			if strings.ContainsRune(allowedChars, v) {
+				newText = append(newText, v)
 			}
 		}
+
 		fmt.Println(string(newText) + "." + tlds[rand.Intn(len(tlds))])
 	}
 }
