a8940d8f6f5b4d0532e71619ba4d3b9c0ad4ed8f
mochi8k <y.hkh.185@gmail.com>
2016-08-19 23:14:21 +0900

    refactoring main.go

--- coolify/main.go
+++ coolify/main.go
@@ -5,16 +5,53 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"strings"
 	"time"
 )
 
-const (
-	duplicateVowel bool = true
-	removeVowel    bool = false
-)
+const vowelString = "aiueoAiueo"
+
+func isVowel(s string) bool {
+	return strings.Contains(vowelString, s)
+}
 
 func randBool() bool {
 	return rand.Intn(2) == 0
+}
+
+func duplicateVowel(word []byte, i int) []byte {
+	if i < 0 {
+		return word
+	}
+	return append(word[:i+1], word[i:]...)
+}
+
+func removeVowel(word []byte, i int) []byte {
+	if i < 0 {
+		return word
+	}
+	return append(word[:i], word[i+1:]...)
+}
+
+func transform(word []byte) []byte {
+	var vI int = -1
+	for i, char := range word {
+		if !isVowel(string(char)) {
+			continue
+		}
+
+		if randBool() {
+			vI = i
+		}
+
+		if randBool() {
+			word = duplicateVowel(word, vI)
+		} else {
+			word = removeVowel(word, vI)
+		}
+
+	}
+	return word
 }
 
 func main() {
@@ -23,28 +60,8 @@ func main() {
 	for s.Scan() {
 		word := []byte(s.Text())
 
-		if !randBool() {
-			fmt.Println(string(word))
-			continue
-		}
-
-		var vI int = -1
-		for i, char := range word {
-			switch char {
-			case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
-				if randBool() {
-					vI = i
-				}
-
-				if vI >= 0 {
-					switch randBool() {
-					case duplicateVowel:
-						word = append(word[:vI+1], word[vI:]...)
-					case removeVowel:
-						word = append(word[:vI], word[vI+1:]...)
-					}
-				}
-			}
+		if randBool() {
+			word = transform(word)
 		}
 
 		fmt.Println(string(word))
